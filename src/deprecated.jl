efca346a752feba2ddf77b5dca696794ed849870
Brian Jackson <bjack205@gmail.com>
2020-04-15 13:05:52 -0700

    Add deprecations for one(::RodriguesVec)

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,7 +1,18 @@
 
 # Deprecate RodriguesVec => RotationVec
-abstract type RodriguesVec end
+struct RodriguesVec{T} end
 @deprecate RodriguesVec(x, y, z) RotationVec(x, y, z)
 @deprecate RodriguesVec(t::NTuple{9}) RotationVec(t::NTuple{9})
 @deprecate RodriguesVec(r::Rotation) RotationVec(r::Rotation)
-# QUESTION: not sure how to deprecate Base.one(::Type{RodriguesVec})
+function RodriguesVec{T}(x, y, z) where T
+    Base.depwarn("`RodriguesVec` is deprecated, use `RotationVec` instead", :Type)
+    RotationVec{T}(x,y,z)
+end
+function Base.one(::Type{RodriguesVec})
+    Base.depwarn("`one(::Type{RodriguesVec})` is deprecated, use `one(::Type{RotationVec})` instead.", :one)
+    one(RotationVec)
+end
+function Base.one(::Type{RodriguesVec{T}}) where T
+    Base.depwarn("`one(::Type{RodriguesVec{T}})` is deprecated, use `one(::Type{RotationVec{T}})` instead.", :one)
+    one(RotationVec{T})
+end
--- test/deprecated.jl
+++ test/deprecated.jl
@@ -1,4 +1,10 @@
 
-@test_deprecated RodriguesVec(1,2,3)
-@test_deprecated RodriguesVec(rand(UnitQuaternion))
-@test_deprecated RodriguesVec(Tuple(rand(UnitQuaternion)))
+@testset "Deprecations" begin
+    # RodriguesVec
+    @test_deprecated RodriguesVec(1,2,3)
+    @test_deprecated RodriguesVec(rand(UnitQuaternion))
+    @test_deprecated RodriguesVec(Tuple(rand(UnitQuaternion)))
+    @test_deprecated RodriguesVec{Float32}(1,2,3)
+    @test_deprecated one(RodriguesVec)
+    @test_deprecated one(RodriguesVec{Float64})
+end
