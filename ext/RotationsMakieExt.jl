783d8cf59f390ab49acc26475469b0b628d70eb5
Martin Kunz <martinkunz@email.cz>
2025-08-28 14:03:16 +0200

    feat(RotationsMakieExt): Add arrows, attributes and use the Compute Pipeline

--- ext/RotationsMakieExt.jl
+++ ext/RotationsMakieExt.jl
@@ -8,79 +8,121 @@ Makie.plottype(::Rotation{2}) = Rotation2Plot
 Makie.plottype(::Rotation{3}) = Rotation3Plot
 
 @recipe Rotation2Plot (rot::Rotation{2},) begin
+    begin
+        arrows_attributes = Makie.documented_attributes(Arrows2D)
+        filter!(a -> a.first ∉ [:color], arrows_attributes.d)
+        arrows_attributes
+    end...
+    "Origin point for the 2D rotation visualization"
     origin = SVector(0, 0)
+    "Size of the reference square"
     boxsize = 0.2
+    "Length of the axis lines"
     axissize = 1.0
+    "Colours for the two axes (x, y)"
     axiscolors = [:red, :green]
+    "Colour of the reference square"
     boxcolor = :gray
 end
 
 function Makie.plot!(p::Rotation2Plot{<:Tuple{<:Rotation{2}}})
-    rot = p[1][]
-    origin = p.origin[]
-    boxsize = p.boxsize[]
-    axissize = p.axissize[]
-    axiscolors = p.axiscolors[]
-    boxcolor = p.boxcolor[]
+    # Use ComputationPipeline for dynamic updates
+    map!(p.attributes, [:rot, :origin, :boxsize, :axissize], [:arrows_origin, :arrows1_end, :arrows2_end, :box_points]) do rot, origin, l, L
+        e₁ = rot[:, 1]
+        e₂ = rot[:, 2]
+        ox, oy = origin
 
-    l = boxsize
-    L = axissize
-    e₁ = rot[:, 1]
-    e₂ = rot[:, 2]
-    ox, oy = origin
+        # Compute box corners
+        box_points = [Point2f(ox, oy) + rot * Point2f(x, y) for x in (-l, l) for y in (-l, l)][[1, 2, 4, 3]]
 
-    # Draw axes
-    lines!(p, [ox, ox + L * e₁[1]], [oy, oy + L * e₁[2]], color=axiscolors[1])
-    lines!(p, [ox, ox + L * e₂[1]], [oy, oy + L * e₂[2]], color=axiscolors[2])
+        return (Point2f(ox, oy), Point2f(ox + L * e₁[1], oy + L * e₁[2]), Point2f(ox + L * e₂[1], oy + L * e₂[2]), box_points)
+    end
 
-    # Draw box
-    points = [SVector(ox, oy) + rot * SVector(x, y) for x in (-l, l) for y in (-l, l)]
-    poly!(p, points[[1, 2, 4, 3]], color=boxcolor)
+    map!(p.attributes, [:axiscolors], [:arrows1_color, :arrows2_color]) do axiscolors
+        (axiscolors[1], axiscolors[2])
+    end
 
-    p
+    arrows2d!(p, p.attributes, p.arrows_origin, p.arrows1_end; color=p.arrows1_color)
+    arrows2d!(p, p.attributes, p.arrows_origin, p.arrows2_end; color=p.arrows2_color)
+    poly!(p, p.attributes, p.box_points; color=p.boxcolor)
+
+    return p
 end
 
 @recipe Rotation3Plot (rot::Rotation{3},) begin
+    begin
+        arrows_attributes = Makie.documented_attributes(Arrows3D)
+        filter!(a -> a.first ∉ [:color], arrows_attributes.d)
+        arrows_attributes
+    end...
+    "Origin point for the 3D rotation visualization"
     origin = SVector(0, 0, 0)
+    "Size of the reference cube"
     boxsize = 0.2
+    "Length of the axis lines"
     axissize = 1.0
+    "Colours for the three axes (x, y, z)"
     axiscolors = [:red, :green, :blue]
+    "Colour of the reference cube faces"
     boxcolor = :gray
+    "Colour of cube edges"
+    edgecolor = :black
 end
 
 function Makie.plot!(p::Rotation3Plot{<:Tuple{<:Rotation{3}}})
-    rot = p[1][]
-    origin = p.origin[]
-    boxsize = p.boxsize[]
-    axissize = p.axissize[]
-    axiscolors = p.axiscolors[]
-    boxcolor = p.boxcolor[]
+    # Use ComputationPipeline for dynamic updates
+    map!(p.attributes, [:rot, :origin, :boxsize, :axissize], [:arrows_origin, :arrows1_end, :arrows2_end, :arrows3_end, :cube_points, :cube_faces, :cube_edges]) do rot, origin, l, L
+        e₁ = rot[:, 1]
+        e₂ = rot[:, 2]
+        e₃ = rot[:, 3]
+        ox, oy, oz = origin
 
-    l = boxsize
-    L = axissize
-    e₁ = rot[:, 1]
-    e₂ = rot[:, 2]
-    e₃ = rot[:, 3]
-    ox, oy, oz = origin
+        # Compute cube vertices
+        cube_points = [Point3f(ox, oy, oz) + rot * Point3f(x, y, z) for x in (-l, l) for y in (-l, l) for z in (-l, l)]
 
-    # Draw axes
-    lines!(p, [ox, ox + L * e₁[1]], [oy, oy + L * e₁[2]], [oz, oz + L * e₁[3]], color=axiscolors[1])
-    lines!(p, [ox, ox + L * e₂[1]], [oy, oy + L * e₂[2]], [oz, oz + L * e₂[3]], color=axiscolors[2])
-    lines!(p, [ox, ox + L * e₃[1]], [oy, oy + L * e₃[2]], [oz, oz + L * e₃[3]], color=axiscolors[3])
+        # Define cube faces (triangulated)
+        cube_faces = [
+            1, 2, 4, 4, 3, 1, # bottom
+            5, 6, 8, 8, 7, 5, # top
+            1, 2, 6, 6, 5, 1, # front
+            3, 4, 8, 8, 7, 3, # back
+            1, 3, 7, 7, 5, 1, # left
+            2, 4, 8, 8, 6, 2  # right
+        ]
 
-    # Draw box
-    points = [Point3f(ox, oy, oz) + rot * Point3f(x, y, z) for x in (-l, l) for y in (-l, l) for z in (-l, l)]
-    faces = [
-        1, 2, 4, 4, 3, 1, # bottom
-        5, 6, 8, 8, 7, 5, # top
-        1, 2, 6, 6, 5, 1, # front
-        3, 4, 8, 8, 7, 3, # back
-        1, 3, 7, 7, 5, 1, # left
-        2, 4, 8, 8, 6, 2  # right
-    ]
-    mesh!(p, points, faces, color=boxcolor)
+        # Define cube edges for wireframe
+        cube_edges = [
+            # Bottom face edges
+            cube_points[1], cube_points[2], cube_points[2], cube_points[4],
+            cube_points[4], cube_points[3], cube_points[3], cube_points[1],
+            # Top face edges
+            cube_points[5], cube_points[6], cube_points[6], cube_points[8],
+            cube_points[8], cube_points[7], cube_points[7], cube_points[5],
+            # Vertical edges
+            cube_points[1], cube_points[5], cube_points[2], cube_points[6],
+            cube_points[3], cube_points[7], cube_points[4], cube_points[8]
+        ]
 
-    p
+        return (
+            Point3f(ox, oy, oz),
+            Point3f(ox + L * e₁[1], oy + L * e₁[2], oz + L * e₁[3]),
+            Point3f(ox + L * e₂[1], oy + L * e₂[2], oz + L * e₂[3]),
+            Point3f(ox + L * e₃[1], oy + L * e₃[2], oz + L * e₃[3]),
+            cube_points, cube_faces, cube_edges
+        )
+    end
+
+    map!(p.attributes, [:axiscolors], [:arrows1_color, :arrows2_color, :arrows3_color]) do axiscolors
+        (axiscolors[1], axiscolors[2], axiscolors[3])
+    end
+
+    arrows3d!(p, p.attributes, p.arrows_origin, p.arrows1_end, color=p.arrows1_color)
+    arrows3d!(p, p.attributes, p.arrows_origin, p.arrows2_end, color=p.arrows2_color)
+    arrows3d!(p, p.attributes, p.arrows_origin, p.arrows3_end, color=p.arrows3_color)
+    mesh!(p, p.attributes, p.cube_points, p.cube_faces; color=p.boxcolor)
+    linesegments!(p, p.attributes, p.cube_edges; color=p.edgecolor)
+
+    return p
 end
 
 Makie.preferred_axis_type(::Rotation3Plot) = Makie.Axis3
